fdc9e732a4f11d4f9551cc838641eb0d1a349194
Alberto Vecchiatti <albertovecchiatti@gmail.com>
2022-11-29 17:40:43 +0100

    Aggiungere i file di progetto.

---
+++ AlgoritmoNumeriPrimi/Program.cs
@@ -0,0 +1,47 @@
+﻿
+//string risposta = "s";
+
+//while(risposta == "s")
+//{
+
+//    Console.Write("Inserisci il numero: ");
+//    int n = int.Parse(Console.ReadLine());
+//    bool isPrimo = true;
+//    for (int i = 2; i < n; i++)
+//    {
+//        if (n % i == 0)
+//        {
+//            isPrimo = false;
+//        }
+            
+//    }
+//    if (isPrimo == true)
+//        Console.WriteLine("Il numero è primo");
+//    else
+//        Console.WriteLine("Il numero non è primo");
+//    Console.ReadKey();
+//}
+
+int numeriPrimi = 0;
+for(int j = 100000; j < 101000; j++)
+{
+    Console.WriteLine(j.ToString());
+    bool isPrimo = true;
+    for(int i = 2; i < j; i++)
+    {
+        if(j % i == 0)
+        {
+            isPrimo = false;
+        }
+    }
+    if (isPrimo == true)
+    {
+        Console.WriteLine("il numero è primo");
+        numeriPrimi = numeriPrimi + 1;
+    }
+    else
+    {
+        Console.WriteLine("il numero non è primo");
+    }
+}
+Console.WriteLine("i numeri primi sono: " + numeriPrimi);   
